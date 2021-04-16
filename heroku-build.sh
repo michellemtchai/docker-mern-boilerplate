7eba6dc1dd4e5d67c6b223b36cceb75387fa9d0d
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-04-15 20:51:57 -0400

    add env to heroku-build

--- heroku-build.sh
+++ heroku-build.sh
@@ -11,5 +11,9 @@ yarn build
 cd ../
 mv ./frontend/build/* ./public
 node ./docker/deploy.js
-source .env
+export APP_ENV=production
+export APP_HOST=0.0.0.0
+export APP_PUBLIC_URL=/assets
+export APP_START_URL=/
+export APP_SERVER_CONSOLE_LOG=0
 rm .env
